c0b46a3589e5dd42d88c73af1976f304d9f43766
natashaborda <78230228+natashaborda@users.noreply.github.com>
2021-02-05 21:56:46 -0500

    Add files via upload

---
+++ PullRequestCreatingVariablesInJavaScript.js
@@ -0,0 +1,37 @@
+let name = 'Natasha';
+
+let age = 14;
+
+let dayOfWeek = 4;
+
+const gender = 'female';
+
+const eyeColor = 'brown';
+
+let numberOfFriends = 2
+
+let todayIsThursday = true;
+
+let tomorrowIsSunday = false; 
+
+let siblings = ['Mario', 'Jasleen', 'Julissa'];
+
+let ages = [18, 11, 10]
+
+let isStudent = true;
+
+const emerson = {
+	name: 'Emerson Middle School'
+	age: 106,
+	numberOfClassrooms: 52
+};
+
+const house = {
+	age: 14
+	color: red
+	rooms: 5
+	bedrooms: 2
+	bathrooms: 1
+	kitchen: 1
+	livingRooms: 1
+};
